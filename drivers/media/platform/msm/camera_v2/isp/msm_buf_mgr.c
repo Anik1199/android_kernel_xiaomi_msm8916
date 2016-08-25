a36edd9d776e6a9bc3927065cad2b0361606cc10
Suman Mukherjee <sumam@codeaurora.org>
2016-08-25 08:58:09 +0530

    msm: camera: isp: Fix warning and errors based on static analysis
    This change fixes the warning/errors from static analysis
    CRs-fixed: 992942
    Signed-off-by: Jing Zhou <jzhou70@codeaurora.org>
    Signed-off-by: Suman Mukherjee<sumam@codeaurora.org>
    Change-Id: Iaf90ab4c1d17f903d03458d76cab1b4c0a5c8836

--- drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014,2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -43,8 +43,10 @@ struct msm_isp_bufq *msm_isp_get_bufq(
 {
 	struct msm_isp_bufq *bufq = NULL;
 	uint32_t bufq_index = bufq_handle & 0xFF;
-	if (bufq_index > buf_mgr->num_buf_q)
-		return bufq;
+	if ((bufq_handle == 0) ||
+		(bufq_index > buf_mgr->num_buf_q) ||
+		(bufq_index >= BUF_MGR_NUM_BUF_Q))
+		return NULL;
 
 	bufq = &buf_mgr->bufq[bufq_index];
 	if (bufq->bufq_handle == bufq_handle)
