5499e2866fbbc2d0213e2b02841f835390c2cdec
Vitaliy Tomin <highwaystar.ru@gmail.com>
2016-11-19 12:48:40 +0800

    drivers: input: Remove unnecessary Xiaomi changes
    Change-Id: Ie4a2ae18a445a2372b28e2942d509dc29fd0a15d

--- include/linux/input/ft5x06_720p.h
+++ include/linux/input/ft5x06_720p.h
@@ -299,8 +299,6 @@ struct ft5x06_ts_data {
 
 #define CTP_CHARGER_DETECT 1
 
-#define CTP_PROC_INTERFACE 1
-#define CTP_LOCKDOWN_INFO  1
 #define BoardId_SUPPORT_FW	 1
 
 
--- include/linux/input/ft5x06_ts.h
+++ include/linux/input/ft5x06_ts.h
@@ -293,9 +293,6 @@ struct ft5x06_ts_data {
 
 #define CTP_CHARGER_DETECT 1
 
-#define CTP_PROC_INTERFACE 1
-#define CTP_LOCKDOWN_INFO  1
-
 #define CTP_DEBUG_ON 1
 #define CTP_DEBUG_FUNC_ON 0
 #define CTP_INFO(fmt, arg...)		   printk("FT5X06-TP-TAG INFO:"fmt"\n", ##arg)
